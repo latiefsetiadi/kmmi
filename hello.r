e9f1f65dbf64bc6c8f2d7537f152398c78a9803b
latiefsetiadi <latiefsetiadi31@gmail.com>
2021-08-09 12:37:42 +0700

    perubahan

--- hello.r
+++ hello.r
@@ -1,7 +1,8 @@
 teks = "Selamat datang di dunia R!"
-teks2 = "Anda akan terkesima dengan kehebatannya!"
+teks2 = " Anda akan terkesima dengan kehebatannya! "
 
 2021 + 18
+
 print(teks2)
-print(teks)
+print(teks) 
 
